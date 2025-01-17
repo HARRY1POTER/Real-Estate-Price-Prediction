152c64baf59cee56ad9a29bb4bf8268a11923205
Anirudhsinh <mrhpotter124@gmail.com>
2025-01-17 12:43:00 +0530

    commit ..

--- Server/util.py
+++ Server/util.py
@@ -6,7 +6,7 @@
 # __locations = None
 # __data_columns = None
 # __ model = None
-
+  
 
 # def get_estimated_price(location, sqft, bhk, bath):
 #     try:
@@ -55,7 +55,7 @@
 #     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
 #     print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
 #     print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
-  
+
 
 import pickle
 import json
